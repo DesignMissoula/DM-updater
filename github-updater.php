0e8c2880894d7b057c998e117881fe316053eef8
Bradford Knowlton <brad@ucanbonline.net>
2014-07-04 15:01:59 -0600

    Initial Commit disable bitbucket classes

--- github-updater.php
+++ github-updater.php
@@ -9,7 +9,7 @@
  */
 
 /*
-Plugin Name:       GitHub Updater
+Plugin Name:       DM Updater
 Plugin URI:        https://github.com/afragen/github-updater
 Description:       A plugin to automatically update GitHub or Bitbucket hosted plugins and themes into WordPress. Plugin class based upon <a href="https://github.com/codepress/github-plugin-updater">codepress/github-plugin-updater</a>. Theme class based upon <a href="https://github.com/WordPress-Phoenix/whitelabel-framework">Whitelabel Framework</a> modifications.
 Version:           2.7.1
@@ -18,7 +18,7 @@ License:           GNU General Public License v2
 License URI:       http://www.gnu.org/licenses/gpl-2.0.html
 Domain Path:       /languages
 Text Domain:       github-updater
-GitHub Plugin URI: https://github.com/afragen/github-updater
+GitHub Plugin URI: https://github.com/DesignMissoula/DM-updater
 GitHub Branch:     master
 */
 
@@ -30,7 +30,7 @@ if ( is_admin() && ( ! defined( 'DOING_AJAX' ) || ! DOING_AJAX ) ) {
 	if ( ! class_exists( 'GitHub_Updater' ) ) {
 		require_once 'includes/class-github-updater.php';
 		require_once 'includes/class-github-api.php';
-		require_once 'includes/class-bitbucket-api.php';
+		// require_once 'includes/class-bitbucket-api.php';
 	}
 	if ( ! class_exists( 'GitHub_Plugin_Updater' ) ) {
 		require_once 'includes/class-plugin-updater.php';
